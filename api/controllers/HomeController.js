6be27f7ade65175faf29d67da83da5649ee4c391
zhangxinglong <zhangxinglong@gbase.cn>
2017-09-15 18:04:49 +0800

    修改policy

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -40,6 +40,9 @@ module.exports = {
     },
 
     index: function (req, res) {
+        if (!req.isAuthenticated()) {
+            return res.redirect('/auth/wechat-enterprise');
+        }
         res.view('homepage');
     },
 
--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -15,9 +15,9 @@ module.exports = function(req, res, next) {
     // (default res.forbidden() behavior can be overridden in `config/403.js`)
     // return res.forbidden('You are not permitted to perform this action.');
     // if (!req.session.authenticated) {
-    if (!req.isAuthenticated()) {
-        return res.redirect('/auth/wechat-enterprise');
-    }
+    // if (!req.isAuthenticated()) {
+    //     return res.redirect('/auth/wechat-enterprise');
+    // }
 
     // User is allowed, proceed to the next policy, 
     // or if this is the last policy, the controller
