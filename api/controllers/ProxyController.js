1a69cdaae2c46181cb0998f8ed8deda276bd132f
zhangxinglong <zhangxinglong@gbase.cn>
2018-06-06 17:52:57 +0800

    todo 代理请求

---
+++ api/controllers/ProxyController.js
@@ -0,0 +1,11 @@
+/**
+ * ProxyController
+ *
+ * @description :: Server-side logic for managing Proxies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
