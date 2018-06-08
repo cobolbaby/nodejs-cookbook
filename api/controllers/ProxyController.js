b1b0f9f0c2733891fba1d1b81240b097e777b15d
zhangxinglong <zhangxinglong@gbase.cn>
2018-06-08 12:40:00 +0800

    style: 完善代理配置

--- api/controllers/ProxyController.js
+++ api/controllers/ProxyController.js
@@ -5,28 +5,34 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-const request = require('request');
+const request = require('request'),
+	ms = require('ms');
 
-function proxy (req, res) {
-    let opts = {
-		method      : req.method,
-		url         : req.url,
-		headers     : req.headers
-	};
-    return req.pipe(request(opts)).on('error', function(err) {
-        if (err) {
-            sails.log.error(`[${req.url}]`, err);
-        }
+function proxy(req, res) {
+	let opts = {
+		method: req.method,
+		url: req.url,
+		headers: req.headers,
+        timeout: ms('60s'),
+        followRedirect: false
+    };
+    // 如何处理请求体Body
+    
+	return req.pipe(request(opts)).on('error', function (err) {
+		if (err) {
+			sails.log.error(`[${req.url}]`, err);
+		}
 	}).pipe(res);
 }
 
-function test (req, res) {
-    const r = request.defaults({'proxy': sails.config.proxyServer});
-    r.get('https://www.google.com/search?q=shujuguan').pipe(res);
+function test(req, res) {
+	const r = request.defaults({
+		'proxy': sails.config.proxyServer
+	});
+	r.get('https://www.google.com/search?q=shujuguan').pipe(res);
 }
 
 module.exports = {
-    proxy,
-    test
+	proxy,
+	test
 };
-
