e1b6cb059d2c85da15cf6efd8ebfb8fb7d9a58c0
liguofang <liguofang@liguofangdeMacBook-Pro.local>
2018-06-21 20:42:11 +0800

    feat: log记录至文本中

--- config/log.js
+++ config/log.js
@@ -11,12 +11,45 @@
  */
 
 const winston = require('winston');
+require('winston-daily-rotate-file');
+const path = require('path');
+const fs = require('fs');
+
+// 自定义日志目录
+const LOG_DIR = 'logs';
+
+//=======================================================================================
+// prepare log directoy before start.
+//=======================================================================================
+((logdir) => {
+
+	const dirname = path.resolve(logdir);
+	console.log('prepare log directoy before start.');
+
+	// check if exist, otherwise create one.
+	try {
+		fs.accessSync(dirname, fs.F_OK)
+	} catch (err) {
+		fs.mkdirSync(dirname);
+	}
+	let fstat = fs.statSync(dirname);
+	if (!fstat.isDirectory()) {
+		throw new Error(`${dirname} is not a directoy`);
+	}
+	// check writable.
+	try {
+		fs.accessSync(dirname, fs.W_OK);
+	} catch (error) {
+		throw new Error(`${dirname} not allowed to write`);
+	}
+
+})(LOG_DIR);
 
 // let option = require('');
 let option = {
 	levels: {},
-    level: 'silly',
-    transports: []
+	level: 'silly',
+	transports: []
 };
 option.levels = {
 	silent: 0,
@@ -27,10 +60,17 @@ option.levels = {
 	verbose: 5,
 	silly: 6
 };
-option.transports = _.extend(option.transports, [
+option.transports = option.transports.concat([
 	new winston.transports.Console({
 		colorize: true,
 		timestamp: true
+	}),
+	new winston.transports.DailyRotateFile({
+		dirname: LOG_DIR,
+		filename: 'application.%DATE%.log',
+		datePattern: 'yyyy-MM-dd',
+		zippedArchive: true,
+		// maxFiles: '14d',
 	})
 ]);
 const logger = new winston.Logger(option);
